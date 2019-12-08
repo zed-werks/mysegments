125ce2f93fb27f3b66b85c336013028297a6b43e
bradhead <brad@zedwerks.com>
2019-12-07 21:16:31 -0800

    added check for Auth

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,19 @@
+//-------------------------------------------------------------------------
+// Copyright © 2019 Zed Werks Inc.
+//
+// Licensed under the GNU General Public License, Version 3.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-------------------------------------------------------------------------
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices;
@@ -46,6 +62,18 @@ namespace mysegments
                 app.UseHsts();
                 app.UseHttpsRedirection();
             }
+
+            app.Use(async (context, next) =>
+            {
+                if (!context.User.Identity.IsAuthenticated)
+                {
+                    await context.ChallengeAsync();
+                }
+                else
+                {
+                    await next();
+                }
+            });
 
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
