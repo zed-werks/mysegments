e39eea126b54d276efb9e2b76d6614d5dbf37382
bradhead <brad@zedwerks.com>
2019-12-09 00:04:48 -0800

    added Strava link

--- ClientApp/src/components/Counter.vue
+++ ClientApp/src/components/Counter.vue
@@ -1,25 +1,29 @@
 <template>
   <div>
-    <p>Current count (Vuex):
+    <p>
+      Current count (Vuex):
       <strong>{{ currentCount }}</strong>
     </p>
     <v-btn class="ma-2" color="info" @click.prevent="increment">Increment</v-btn>
     <v-btn class="ma-2" color="info" @click.prevent="reset">Reset</v-btn>
+    <p>
+      <a href="/Strava/Connect?returnUrl=/">Connect with Strava</a>
+    </p>
   </div>
 </template>
 
 <script lang="ts">
-import { Action, Getter } from 'vuex-class';
-import { Component, Vue } from 'vue-property-decorator';
-const namespace: string = 'counter';
+import { Action, Getter } from "vuex-class";
+import { Component, Vue } from "vue-property-decorator";
+const namespace: string = "counter";
 
 @Component
 export default class Counter extends Vue {
-  @Getter('currentCount', { namespace })
+  @Getter("currentCount", { namespace })
   private currentCount!: number;
-  @Action('increment', { namespace })
+  @Action("increment", { namespace })
   private incrementCounter: any;
-  @Action('reset', { namespace })
+  @Action("reset", { namespace })
   private resetCounter: any;
 
   private increment() {
